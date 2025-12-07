f35184499119b65bda5adb9a5f205c8e4d68cc39
Srishti <srishtichoudhary52@gmail.com>
2025-12-07 20:14:29 +0530

    matrix effect

---
+++ Task 8 (matrix effect on letters)/script.js
@@ -0,0 +1,18 @@
+const p = document.querySelector("p")
+const text = p.innerText
+
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+let iteration = 0;
+function randomText(){
+    const str = text.split("").map((char,index)=>{
+        if(index<iteration){
+            return char
+        }
+        return characters.split("")[Math.floor(Math.random()*52)]
+    }).join("")
+    p.innerText = str
+    iteration += 0.2
+
+}
+setInterval(randomText,30);
