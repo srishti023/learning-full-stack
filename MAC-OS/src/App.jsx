a4aa7f8102030c155f85b124276dfb4097a7ef27
Srishti <srishtichoudhary52@gmail.com>
2026-07-14 14:35:02 +0530

    changes

--- MAC-OS/src/App.jsx
+++ MAC-OS/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './app.scss'
 import Dock from './components/Dock'
 import Nav from './components/Nav'
+import MacWindow from './components/windows/MacWindow'
 
 function App() {
 
@@ -9,6 +10,8 @@ function App() {
     <main>
       <Nav />
       <Dock />
+
+      <MacWindow />
     </main>
   )
 }
---
+++ MAC-OS/src/components/windows/MacWindow.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Rnd} from 'react-rnd'
+import "./window.scss"
+
+const MacWindow = () => {
+  return (
+    <Rnd>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt optio laboriosam unde sint aliquam animi. Quisquam nemo minima assumenda quaerat odio. Nulla, enim. Eligendi laboriosam doloremque dolores vitae omnis id.
+</Rnd>
+  )
+}
+
+export default MacWindow
